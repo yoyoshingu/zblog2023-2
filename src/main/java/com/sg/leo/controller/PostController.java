d871498ed9d8f4c6598a67d462e40c64fb842c0d
Y Lee <leo@shingu.ac.kr>
2023-06-15 14:19:05 +0900

    게시물보이기완성

--- src/main/java/com/sg/leo/controller/PostController.java
+++ src/main/java/com/sg/leo/controller/PostController.java
@@ -3,7 +3,9 @@ package com.sg.leo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.sg.leo.domain.Post;
 import com.sg.leo.domain.User;
 import com.sg.leo.dto.ResponseDTO;
+import com.sg.leo.repository.PostRepository;
 import com.sg.leo.service.PostService;
 
 import jakarta.servlet.http.HttpSession;
@@ -18,7 +21,8 @@ import jakarta.servlet.http.HttpSession;
 @Controller
 public class PostController {
 	@GetMapping({"/", ""})
-	public String getPostList() {
+	public String getPostList(Model model) {
+		model.addAttribute("postList", postService.getPostList());
 		return "zblogindex";
 	}
 	
@@ -38,4 +42,11 @@ public class PostController {
 		postService.insertPost(post);
 		return new ResponseDTO<>(HttpStatus.OK.value(), "새로운포스트 등록");
 	}
+	
+	@GetMapping("/post/{id}")
+	public String getPost( @PathVariable int id, Model model) {
+		model.addAttribute("thepost", postService.getPost(id));
+		return "post/getPost";
+	}
+	
 }
--- src/main/java/com/sg/leo/service/PostService.java
+++ src/main/java/com/sg/leo/service/PostService.java
@@ -1,5 +1,7 @@
 package com.sg.leo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +17,16 @@ public class PostService {
 	@Transactional
 	public void insertPost(Post post) {
 		postRepository.save(post);
+	}
+	
+	@Transactional(readOnly = true)
+	public List<Post> getPostList() {
+		return postRepository.findAll();
+	}
+	
+	@Transactional(readOnly = true)
+	public Post getPost(int id) {
+		return postRepository.findById(id).get();
 	}
 
 }
